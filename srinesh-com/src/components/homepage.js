34cacb692ef3aee1171ae4749fd52bfccdb08d1e
srinashg <51180003+srinashg@users.noreply.github.com>
2024-12-13 19:42:01 -0600

    completed navigation bar

---
+++ srinesh-com/src/components/homepage.js
@@ -0,0 +1,14 @@
+
+/*
+function HomePage() {
+    return (
+      <div>
+        <title>Home Page</title>
+        <h1>Home Page</h1>
+        <p>Home Page</p>
+      </div>
+    );
+  }
+  
+  export default HomePage;
+*/
--- srinesh-com/src/index.js
+++ srinesh-com/src/index.js
@@ -1,3 +1,4 @@
+import 'bootstrap/dist/css/bootstrap.min.css'; //importing bootstrap css to display the navbar instead of just basic html content
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -7,6 +8,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    // The App component is now rendered within a root - index.js is where the app is run
     <App />
   </React.StrictMode>
 );
